83110ae79b5db8e3100af8c873b83694d97344cc
Project4245 <130088086+Project4245@users.noreply.github.com>
2023-10-30 21:28:00 +0530

    Add files via upload

---
+++ src/main/java/com/bmd/model/JwtRequest.java
@@ -0,0 +1,19 @@
+package com.bmd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtRequest {
+
+	private String password;
+
+	private String phoneNumber;
+
+//	private String userId;
+}
