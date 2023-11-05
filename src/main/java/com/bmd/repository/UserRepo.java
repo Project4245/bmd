dc4e31af04b707972272a87ac23be1c0587d4fee
Project4245 <130088086+Project4245@users.noreply.github.com>
2023-11-05 15:48:49 +0530

    Add files via upload

---
+++ src/main/java/com/bmd/repository/UserRepo.java
@@ -0,0 +1,13 @@
+package com.bmd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bmd.entity.Users;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+	Users findByMobileNumber(String mobileNumber);
+
+}
